eeaf01e8df23d9b861947af9280be11efa457530
mborsch <matthewmborschnack@gmail.com>
2022-06-18 23:20:06 -0500

    routes for movies

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,6 +1,14 @@
 const router = require("express").Router({ mergeParams: true });
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const { Router } = require("express");
+
+/*
+Router.route("/:movieId/theaters").get().all(methodNotAllowed);
+
+Router.route("/:movieId/reviews");
+*/
+router.route(`/:movieId`).get(controller.read).all(methodNotAllowed);
 
 router.route("/").get(controller.list).all(methodNotAllowed);
 
