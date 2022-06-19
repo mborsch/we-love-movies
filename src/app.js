28dcf05267fd4c3de42e43dfab163d01be99fbf8
mborsch <matthewmborschnack@gmail.com>
2022-06-18 23:20:56 -0500

    added reviewsRouter

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const moviesRouter = require("./movies/movies.router");
+const reviewsRouter = require("./reviews/reviews.router");
 
 const app = express();
 
 app.use("/movies", moviesRouter);
-
+app.use("/reviews", reviewsRouter);
 module.exports = app;
