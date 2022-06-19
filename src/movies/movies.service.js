fd2ccfcc5d9b4b564721eef55841531bfd48e0c3
mborsch <matthewmborschnack@gmail.com>
2022-06-18 23:19:52 -0500

    added read and listInTheaters

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -6,6 +6,19 @@ function list() {
   return knex("movies").select("*");
 }
 
+function listInTheaters() {
+  return knex("movies as m")
+    .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+    .select("m.*", "mt.movie_id")
+    .where({ "mt.is_showing": true });
+}
+
+function read(movie_id) {
+  return knex("movies").select("*").where({ movie_id }).first();
+}
+
 module.exports = {
   list,
+  listInTheaters,
+  read,
 };
