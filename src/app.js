3dbd43f7f2f688280a27c17f00df66528e51e48c
mborsch <matthewmborschnack@gmail.com>
2022-06-19 13:48:46 -0500

    added notFound and errorHandler to app.js

--- src/app.js
+++ src/app.js
@@ -3,9 +3,15 @@ const express = require("express");
 const cors = require("cors");
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
 
 const app = express();
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
 module.exports = app;
