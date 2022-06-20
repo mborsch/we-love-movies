412b87979968091f05e41d1ec0d6f0a3e6a81b50
mborsch <matthewmborschnack@gmail.com>
2022-06-19 19:44:45 -0500

    added solution to get all theaters

--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router({ mergeParams: true });
 
-const controller = require("./theaters.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+
+const controller = require("./theaters.controller");
+
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
